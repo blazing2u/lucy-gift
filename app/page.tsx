8310f2844b09cf3bd6d132f83b2a022a4540310f
blazing2u <96360099+blazing2u@users.noreply.github.com>
2026-03-10 02:22:27 -0400

    f

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold text-green-500">hello world</h1>
+      <h1 className="text-4xl font-bold text-green-500">arpan</h1>
     </div>
   )
 }
